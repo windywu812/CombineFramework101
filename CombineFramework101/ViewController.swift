671d135cec20f4d6263b7c698171218c65163391
Windy <windywu812@gmail.com>
2021-07-06 11:10:23 +0700

    update

--- CombineFramework101/ViewController.swift
+++ CombineFramework101/ViewController.swift
@@ -6,14 +6,72 @@
 //
 
 import UIKit
+import Combine
 
 class ViewController: UIViewController {
-
+    
+    @Published var xValue: Int = 0
+    private var cancelables = Set<AnyCancellable>()
+    
+    private var totalLabel: UILabel! = {
+        let v = UILabel()
+        v.font = .preferredFont(forTextStyle: .title1)
+        v.textAlignment = .center
+        return v
+    }()
+    
+    private var xLabel: UITextField = {
+        let v = UITextField()
+        v.translatesAutoresizingMaskIntoConstraints = false
+        v.borderStyle = .roundedRect
+        v.textAlignment = .center
+        return v
+    }()
+        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setupUI()
+        setupAction()
+        setupBinding()
+        
+//        setupActionImperactive()
     }
 
+    private func setupUI() {
+        let stack = UIStackView(arrangedSubviews: [totalLabel, xLabel])
+        stack.spacing = 32
+        stack.axis = .vertical
+        view.addSubview(stack)
+        
+        stack.translatesAutoresizingMaskIntoConstraints = false
+        stack.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        stack.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        
+        xLabel.widthAnchor.constraint(equalToConstant: 100).isActive = true
+    }
+    
+    // MARK: - Reactive Way
+    private func setupAction() {
+        xLabel.addAction(UIAction(handler: { [weak self] _ in
+            self?.xValue = Int(self?.xLabel.text ?? "0") ?? 0
+        }), for: .editingChanged)
+    }
+    
+    private func setupBinding() {
+        $xValue
+            .sink { [weak self] x in
+                self?.totalLabel.text = "\(x)"
+            }.store(in: &cancelables)
+    }
+    
+    // MARK: - Imperative Way (Common Way)
+    private func setupActionImperactive() {
+        xLabel.addAction(UIAction(handler: { [weak self] _ in
+            let value = Int(self?.xLabel.text ?? "0") ?? 0
+            self?.totalLabel.text = "\(value)"
+        }), for: .editingChanged)
+    }
 
 }
 
